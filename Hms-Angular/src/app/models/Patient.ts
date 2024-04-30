1e2255f6ce5d97a0771fb19dd5d39cc7123e5f40
AmitChawarekar <amit.chawarekar@gmail.com>
2024-04-30 16:19:55 +0530

    Project structure updated

---
+++ Hms-Angular/src/app/models/Patient.ts
@@ -0,0 +1,10 @@
+export class Patient{
+    public patientId: any;
+    public patientFirstName:any;
+    public patientLastName:any;
+    public patientPassword:any;
+    public patientDateOfBirth:any;
+    public patientEmail:any;
+    public diseaseCategory:any;
+
+}
---
+++ Hms-Angular/src/app/module/patient-module/patient-module.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PatientRegistrationComponent } from './components/patient-registration/patient-registration.component';
+
+
+
+@NgModule({
+  declarations: [
+    PatientRegistrationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PatientModuleModule { }
