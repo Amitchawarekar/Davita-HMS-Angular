0ff239ef406bcba9c1e359ac3864440b995f15ef
Jahan Patil <Jahan.Patil@citiustech.com>
2024-05-07 18:20:24 +0530

    MODULES

---
+++ Hms-Angular/src/app/models/Nurse.ts
@@ -0,0 +1,10 @@
+export class Nurse{
+    public nurseId: any;
+    public nurseFirstName:any;
+    public nurseLastName:any;
+    public nursePassword:any;
+    public nurseDateOfBirth:any;
+    public nurseEmail:any;
+    public diseaseCategory:any;
+
+}
---
+++ Hms-Angular/src/app/models/Physician.ts
@@ -0,0 +1,10 @@
+export class Physician{
+    public physicianId: any;
+    public physicianFirstName:any;
+    public physicianLastName:any;
+    public physicianPassword:any;
+    public physicianDateOfBirth:any;
+    public physicianEmail:any;
+    public diseaseCategory:any;
+
+}
